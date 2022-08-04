bcd51cc51b455fb02c8544256e070f8747bb8b32
Maria Maksimova <maryia.maksimava@yahoo.com>
2022-08-04 11:44:45 +0400

    Create some interactivity

--- main.py
+++ main.py
@@ -12,12 +12,30 @@ Temp: 68 F
 (5/9) * (68 - 32) = 20 C
 
 """
+import argparse # For parsing arguments when run script
+
 
 
 def cel_to_fahren(tempreture_in_celsium):
     fahren = (tempreture_in_celsium * (9 / 5)) + 32
     return fahren
 
-celsium = float(input("Provide tempruture: "))
+def fahren_to_cel(temprutere_in_fahren):
+    cels = (5 / 9) * (temprutere_in_fahren - 32)
+    return cels
 
-print(cel_to_fahren(celsium))
+
+answer = str(input("What teamprute you want convert?: ")).lower()
+
+if answer in ['celsius', 'cels', 'cel']:  # Convert from Celsius to Fahrenheit
+    celsium = float(input("Provide tempruture in Celsius: "))
+    print(cel_to_fahren(celsium))
+
+elif answer in ['fahrenheit', 'fahren', 'far']: # Convert from Fahrenheit to Celsius
+    fahrenheit = float(input("Provide tempruture in Fahrenheit: "))
+    print(fahren_to_cel(fahrenheit))
+
+else:
+    pass
+
+
