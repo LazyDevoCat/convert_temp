b77369199a05959e5b58e7651d797c5f85de39f9
Maria Maksimova <maryia.maksimava@yahoo.com>
2022-08-05 19:30:40 +0400

    Comments were fixed (I hope)

--- main.py
+++ main.py
@@ -1,35 +1,33 @@
 """
-import argparse - For parsing arguments when run script
-
-function def cel_to_fahren(tempreture_in_celsium): - Take temperature in Celsius and convert to Fahrenheit
-From Celsius  to Fahrenheit
-Temp: 20 C
-20 * (9/5) + 32 = 68 F
-
-
-function def fahren_to_cel(temprutere_in_fahren): - Take temperature in Fahrenheit and convert to Celsius
-From Fahrenheit to Celsius
-Temp: 68 F
-(5/9) * (68 - 32) = 20 C
-
+Main file
 """
 import argparse
 
 
 def cel_to_fahren(tempreture_in_celsium):
+    """
+    Take temperature in Celsius and convert to Fahrenheit
+    Temp: 20 C
+    20 * (9/5) + 32 = 68 F
+    """
     fahren = (tempreture_in_celsium * (9 / 5)) + 32
     return fahren
 
 
 def fahren_to_cel(temprutere_in_fahren):
+    """
+    Take temperature in Fahrenheit and convert to Celsius 
+    Temp: 68 F
+    (5/9) * (68 - 32) = 20 C
+    """
     cels = (5 / 9) * (temprutere_in_fahren - 32)
     return cels
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-v", "--verbose", help="It just return your argument")
-parser.add_argument("-f", "--fahrenheit", help="Take temperature in Fahrenheit and convert to Celsius")
-parser.add_argument("-c", "--celsius", help="Take temperature in Celsius and convert to Fahrenheit")
+parser.add_argument("-f", "--fahrenheit", help="Take temperature in Fahrenheit and convert to Celsius", type=float)
+parser.add_argument("-c", "--celsius", help="Take temperature in Celsius and convert to Fahrenheit", type=float)
 
 args = parser.parse_args()
 
@@ -37,10 +35,10 @@ if args.verbose:
     print(str(args) + " It's verbose argument")
 elif args.fahrenheit:
     # print(f"{args.fahrenheit} Fahrenheit")
-    print(fahren_to_cel(float(args.fahrenheit)))
+    print(fahren_to_cel(args.fahrenheit))
 elif args.celsius:
     # print(f"{args.celsius} Celsius")
-    print(cel_to_fahren(float(args.celsius)))
+    print(cel_to_fahren(args.celsius))
 else:
     answer = str(input("What teamprute you want convert?: ")).lower()
     
