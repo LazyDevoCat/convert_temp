718825499661d51842064ddc166e020d5101ca3c
Maria Maksimova <maryia.maksimava@yahoo.com>
2022-08-05 19:17:53 +0400

    Comments were fixed

--- main.py
+++ main.py
@@ -1,45 +1,46 @@
 """
-You need to create a github repo (public), plan project structure, use MIT license. 
-Also you need to decide how to run two different conversions with a single script, e.g. how to decide which conversion you have to perform.
+import argparse - For parsing arguments when run script
 
+function def cel_to_fahren(tempreture_in_celsium): - Take temperature in Celsius and convert to Fahrenheit
 From Celsius  to Fahrenheit
 Temp: 20 C
 20 * (9/5) + 32 = 68 F
 
 
+function def fahren_to_cel(temprutere_in_fahren): - Take temperature in Fahrenheit and convert to Celsius
 From Fahrenheit to Celsius
 Temp: 68 F
 (5/9) * (68 - 32) = 20 C
 
 """
-import argparse  # For parsing arguments when run script
+import argparse
 
 
-def cel_to_fahren(tempreture_in_celsium):  # Take temperature in Celsius and convert to Fahrenheit
+def cel_to_fahren(tempreture_in_celsium):
     fahren = (tempreture_in_celsium * (9 / 5)) + 32
     return fahren
 
 
-def fahren_to_cel(temprutere_in_fahren):  # Take temperature in Fahrenheit and convert to Celsius
+def fahren_to_cel(temprutere_in_fahren):
     cels = (5 / 9) * (temprutere_in_fahren - 32)
     return cels
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-v", "--verbose", help="It just return your argument")
-parser.add_argument("-f", help="Take temperature in Fahrenheit and convert to Celsius")
-parser.add_argument("-c", help="Take temperature in Celsius and convert to Fahrenheit")
+parser.add_argument("-f", "--fahrenheit", help="Take temperature in Fahrenheit and convert to Celsius")
+parser.add_argument("-c", "--celsius", help="Take temperature in Celsius and convert to Fahrenheit")
 
 args = parser.parse_args()
 
 if args.verbose:
     print(str(args) + " It's verbose argument")
-elif args.f:
-    # print(f"{args.f} Fahrenheit")
-    print(fahren_to_cel(int(args.f)))
-elif args.c:
-    # print(f"{args.c} Celsius")
-    print(cel_to_fahren(int(args.c)))
+elif args.fahrenheit:
+    # print(f"{args.fahrenheit} Fahrenheit")
+    print(fahren_to_cel(float(args.fahrenheit)))
+elif args.celsius:
+    # print(f"{args.celsius} Celsius")
+    print(cel_to_fahren(float(args.celsius)))
 else:
     answer = str(input("What teamprute you want convert?: ")).lower()
     
@@ -50,6 +51,5 @@ else:
     elif answer in ['fahrenheit', 'fahren', 'far', '-f']:  # Convert from Fahrenheit to Celsius
         fahrenheit = float(input("Provide tempruture in Fahrenheit: "))
         print(fahren_to_cel(fahrenheit))
-    
     else:
-        pass
+        print("You provide nothing :) ")
