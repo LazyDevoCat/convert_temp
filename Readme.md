c5bf8f5d4f970420dd9319e2fae754b5b385877a
Maria Maksimova <maryia.maksimava@yahoo.com>
2022-08-06 20:52:56 +0400

    Some cosmetic fixes were added

--- Readme.md
+++ Readme.md
@@ -1,9 +1,9 @@
 ## Description
 
-This is a training and simple (maybe) project for education
+This is a training and simple (maybe) project for education  
 This is a cli tool for converting temperature from Celsius to Fahrenheit and vise versa
 
 ## Tasks
-Create a Github repo (public), plan project structure, use MIT license. 
-Need to decide how to run two different conversions with a single script, e.g. how to decide which conversion you have to perform.
+Create a Github repo (public), plan project structure, use MIT license.  
+Need to decide how to run two different conversions with a single script, e.g. how to decide which conversion you have to perform.  
 Ability to use this tool in CLI and interactive mode.
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ CELSIUM_INPUT = ['celsius', 'cels', 'cel', '-c', 'c']
 FAH_INPUT = ['fahrenheit', 'fahren', 'far', '-f', 'f']
 QUIT_INPUT = ['quit', 'q']
 
+
 def cel_to_fahren(tempreture_in_celsium):
     """
     Take temperature in Celsius and convert to Fahrenheit
@@ -43,20 +44,15 @@ elif args.celsius:
 else:
     answer = str(input("What teamprute you want convert?: ")).lower()
 
-    while answer not in (CELSIUM_INPUT and FAH_INPUT):
+    while answer not in (CELSIUM_INPUT + FAH_INPUT):
         answer = str(input("What teamprute you want convert?: ")).lower()
         if answer in QUIT_INPUT:
             break
-    
-    
+
     if answer in CELSIUM_INPUT:  # Convert from Celsius to Fahrenheit
         celsium = float(input("Provide tempruture in Celsius: "))
         print(cel_to_fahren(celsium))
-    
-    elif answer in FAH_INPUT:  # Convert from Fahrenheit to Celsius
+
+    if answer in FAH_INPUT:  # Convert from Fahrenheit to Celsius
         fahrenheit = float(input("Provide tempruture in Fahrenheit: "))
         print(fahren_to_cel(fahrenheit))
-
-    
-    else:
-        print("You provide nothing :) ")
