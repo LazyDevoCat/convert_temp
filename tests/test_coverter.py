eca242da1d03d94023b13adb005e0fc96bafc7ec
Maria Maksimova <maryia.maksimava@yahoo.com>
2022-08-15 18:37:57 +0400

    Tests were added. Branch issue-5

--- tests/test_coverter.py
+++ tests/test_coverter.py
@@ -2,31 +2,50 @@
 import unittest
 
 from converter.converter import *
+from converter.utils import is_float
 
 
 class test_temperature_conversion(unittest.TestCase):
     
-    def test_cel_to_fahren(self):
+    def test_celsius_to_fahrenheit(self):
         """
-        # Testing function cel_to_fahren
+        Testing function celsius_to_fahrenheit
         """
-        self.assertEqual(cel_to_fahren(20), 68.0)
-        self.assertEqual(cel_to_fahren(20.0), 68.0)
-        self.assertEqual(cel_to_fahren(0), 32.0)
-        self.assertEqual(cel_to_fahren(-10), 14.0)
-        self.assertEqual(cel_to_fahren(-10.0), 14.0)
-        self.assertEqual(cel_to_fahren(-40.0), -40.0)
+        self.assertEqual(celsius_to_fahrenheit(20), 68.0)
+        self.assertEqual(celsius_to_fahrenheit(20.0), 68.0)
+        self.assertEqual(celsius_to_fahrenheit(0), 32.0)
+        self.assertEqual(celsius_to_fahrenheit(-10), 14.0)
+        self.assertEqual(celsius_to_fahrenheit(-10.0), 14.0)
+        self.assertEqual(celsius_to_fahrenheit(-40.0), -40.0)
     
-    def test_fahren_to_cel(self):
+    def test_fahrenheit_to_celsius(self):
         """
-        # Testing function fahren_to_cel
+        Testing function fahrenheit_to_celsius
         """
-        self.assertEqual(fahren_to_cel(32), 0.0)
-        self.assertEqual(fahren_to_cel(32.0), 0.0)
-        self.assertEqual(fahren_to_cel(0), -17.8, msg='ERROR')
-        self.assertEqual(fahren_to_cel(-13), -25.0)
-        self.assertEqual(fahren_to_cel(-13.0), -25.0)
-        self.assertEqual(fahren_to_cel(-40), -40.0)
+        self.assertEqual(fahrenheit_to_celsius(32), 0.0)
+        self.assertEqual(fahrenheit_to_celsius(32.0), 0.0)
+        self.assertEqual(fahrenheit_to_celsius(0), -17.8, msg='ERROR')
+        self.assertEqual(fahrenheit_to_celsius(-13), -25.0)
+        self.assertEqual(fahrenheit_to_celsius(-13.0), -25.0)
+        self.assertEqual(fahrenheit_to_celsius(-40), -40.0)
+
+    def test_is_float(self):
+        """
+        Testing function is_float
+        """
+        self.assertTrue(is_float(0), msg="Failed")
+        self.assertTrue(is_float(0.0))
+        self.assertTrue(is_float(-0.0))
+        self.assertTrue(is_float(10))
+        self.assertTrue(is_float(-10.55))
+        
+        self.assertFalse(is_float("asa"))
+        self.assertFalse(is_float("ass1000asa"))
+        self.assertTrue(is_float("10.0"))
+        self.assertTrue(is_float("-0.0"))
+        self.assertTrue(is_float("   -0.0  "))
+        self.assertTrue(is_float("\u0038"))  # Unicode char for 8
+        self.assertFalse(is_float("\u0038\u0061"))  # Unicode char 8a
 
 
 if __name__ == '__main__':
