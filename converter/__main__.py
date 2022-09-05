6f8c9b6f260a5695fafc21f84ea2163410a9ddb9
Maria Maksimova <maryia.maksimava@yahoo.com>
2022-09-05 12:58:22 +0400

    some refactoring

--- converter/__main__.py
+++ converter/__main__.py
@@ -4,9 +4,9 @@ from converter.exception import TemperatureError, ConverterError
 from converter.factory import ConverterContextFactory
 
 converter_context = ConverterContextFactory().create()
-#converter_function = converter_context.get_converter()
 try:
+    converter_function = converter_context.get_converter()
     temperature = converter_context.get_temperature()
-    print(converter_context.get_converter()(temperature))
+    print(converter_function(temperature))
 except (TemperatureError, ConverterError, ValueError) as error:
     print(f"{error}")
