bce238b28a8505e63469b4edb5b9856582726a88
Maria Maksimova <maryia.maksimava@yahoo.com>
2022-09-05 12:48:50 +0400

    Remove biltin error

--- converter/__init__.py
+++ converter/__init__.py
@@ -1,5 +1,3 @@
-from builtins import ValueError
-
 from .converter import celsius_to_fahrenheit, fahrenheit_to_celsius, ConverterContext
 from .exception import TemperatureError, ConverterError
 from .utils import is_float
@@ -7,6 +5,6 @@ from .utils import is_float
 
 __all__ = [
     "celsius_to_fahrenheit", "fahrenheit_to_celsius", "is_float",
-    "TemperatureError", "ConverterError", "ValueError",
+    "TemperatureError", "ConverterError",
     "ConverterContext"
 ]
--- converter/__main__.py
+++ converter/__main__.py
@@ -4,9 +4,9 @@ from converter.exception import TemperatureError, ConverterError
 from converter.factory import ConverterContextFactory
 
 converter_context = ConverterContextFactory().create()
-converter_function = converter_context.get_converter()
+#converter_function = converter_context.get_converter()
 try:
     temperature = converter_context.get_temperature()
-    print(converter_function(temperature))
+    print(converter_context.get_converter()(temperature))
 except (TemperatureError, ConverterError, ValueError) as error:
     print(f"{error}")
